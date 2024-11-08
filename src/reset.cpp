a0092a927750aa359900dfbc534aa91b7048a272
kuri <182177083+kuri227@users.noreply.github.com>
2024-11-08 18:42:09 +0900

    Create reset.cpp

---
+++ src/reset.cpp
@@ -0,0 +1,14 @@
+// #include "M5StickCPlus2.h"
+// #include "BluetoothSerial.h"
+// #include "MadgwickAHRS.h"
+
+// void setup(){
+//     pinMode(26,INPUT_PULLUP);//スイッチがオンのとき0,スイッチがオフのとき1
+//     Serial.begin(115200);
+// }
+
+// void update(){
+//     bool sw = digitalRead(26);
+
+//     Serial.print(!sw);
+// }
